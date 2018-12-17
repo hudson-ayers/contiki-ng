f19a8fa374e80511dc231d473598d7605223a6f7
Hudson Ayers <hayers@stanford.edu>
2018-12-16 18:02:11 -0800

    Enable bootloader for launchpad to allow for cli flashing

--- arch/cpu/cc26xx-cc13xx/cc13xx-cc26xx-conf.h
+++ arch/cpu/cc26xx-cc13xx/cc13xx-cc26xx-conf.h
@@ -197,7 +197,7 @@
  * @{
  */
 #ifndef ROM_BOOTLOADER_ENABLE
-#define ROM_BOOTLOADER_ENABLE              0
+#define ROM_BOOTLOADER_ENABLE              1
 #endif
 /** @} */
 /*---------------------------------------------------------------------------*/
